8a032623aa017b219ad90333e5fe277713f77549
Sean Dougherty <sean.dougherty@oakland.k12.mi.us>
2026-07-23 14:35:07 -0400

    Configure direct GitHub dashboard persistence

--- config.js
+++ config.js
@@ -1,4 +1,7 @@
 window.EXPLORATION_ASSET_CONFIG = {
-  apiBase: "",
+  githubOwner: "SCconsultant33",
+  githubRepo: "Exploration-Asset-Tracking",
+  githubPath: "data/assets.json",
+  allowedLogin: "SCconsultant33",
   registryUrl: "https://raw.githubusercontent.com/SCconsultant33/Exploration-Asset-Tracking/main/data/assets.json"
 };
